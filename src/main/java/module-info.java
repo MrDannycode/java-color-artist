8104a01dc37df19d950c0a3bdeedcc228d92920a
dan1el <florenta.daniel@gmail.com>
2026-05-20 01:33:47 +0300

    am sters module-info

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.colorartist {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.colorartist to javafx.fxml;
-    exports com.example.colorartist;
-}
